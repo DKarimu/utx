a48f54b78cb8607bbe737b9467419c043b69f297
kdarouy <kdarouy@croozbl.co.jp>
2024-02-06 09:48:00 +0900

    Changes to be committed: 	modified:   docker/Dockerfile 	modified:   docker/docker-compose.yml 	modified:   torun.sh

--- torun.sh
+++ torun.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Load environment variables from .env file
-if [ -f .env ]; then
-  export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
-fi
+# Verify database connection
+check_db_connection() {
+  psql -h "$DJANGO_DB_HOST" -p "$DJANGO_DB_PORT" -U "$DJANGO_DB_USER" -d "$DJANGO_DB_NAME" -c "SELECT 1;" >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "Database connection successful!"
+  else
+    echo "Error: Unable to connect to the database."
+    exit 1
+  fi
+}
+
+# Check database connection
+check_db_connection
 
 # Migrate the database
 python manage.py migrate
